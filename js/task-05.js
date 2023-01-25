b7d9c56396914b84c337f7d2ec0b959768c64355
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-01-26 00:20:44 +0200

    that's it 5 task

--- js/task-05.js
+++ js/task-05.js
@@ -5,15 +5,23 @@
 const inputEl = document.querySelector("#name-input");
 const outPutEl = document.querySelector("#name-output");
 
+// console.dir(inputEl);
+// console.log(inputEl.attributes.placeholder.value);
+// console.log(inputEl.value);
 
-outPutEl.textContent = inputEl.attributes.text;
+if (inputEl.value === "") {
+    outPutEl.textContent = inputEl.attributes.placeholder.value;
+} else {
+    outPutEl.textContent = inputEl.value; 
+};
 
 
 const inputText = (text) => 
 
     (text.currentTarget.value === '') ?
-    outPutEl.textContent = 'Anonymous':
+    outPutEl.textContent = 'Anonymous!!':
     outPutEl.textContent = text.currentTarget.value;
+    console.log(inputEl.value);
 
 
 inputEl.addEventListener("input", inputText );
